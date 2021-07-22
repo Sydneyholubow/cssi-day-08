ff29648cf4a6dd83e71ffc6259a37f4221a043c7
sydneyholubow <sydneyhholubow@gmail.com>
2021-07-22 17:15:28 +0000

    viewing messages working

---
+++ public/js/viewMessages.js
@@ -0,0 +1,20 @@
+console.log("got here")
+
+const getMessages = () => {
+    const passcode = document.querySelector("#passcode")
+    console.log("button pressed", passcode.value)
+
+    const messagesRef = firebase.database().ref();
+    messagesRef.on('value', (snapshot) => {
+        const data = snapshot.val()
+        console.log(data)
+        for (let key in data) {
+            console.log(key, data[key])
+            if (key === passcode.value) {
+                console.log("match found")
+                const message = document.querySelector("#message")
+                message.innerHTML = data[key]
+            }
+        }
+    })
+}
