baf13bccbaa6f4179fa2c72489459aeb6bcd701d
sydneyholubow <sydneyhholubow@gmail.com>
2021-07-22 17:30:59 +0000

    console.logs removed

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -2,13 +2,11 @@ console.log("got here")
 
 const getMessages = () => {
     const passcode = document.querySelector("#passcode")
-    console.log("button pressed", passcode.value)
 
     const messagesRef = firebase.database().ref();
-    console.log(messagesRef)
     messagesRef.on('value', (snapshot) => {
         const data = snapshot.val()
-        console.log(data)
+
         for (let key in data) {
             console.log(key, data[key])
             if (key === passcode.value) {
